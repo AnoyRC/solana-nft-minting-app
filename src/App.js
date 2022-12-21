e0ebc5c804da5a5846b3452a9b01eeaea80dfb31
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2022-12-21 13:35:49 +0530

    Connect Wallet functionalities Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const[walletAddress, setWalletAddress] = useState(null)
+
+  const checkIfWalletConnected = async() => {
+    try {
+      const {solana} = window;
+      if(solana){
+        if(solana.isPhantom){
+          console.log("Phantom Wallet found !")
+          const response = await solana.connect({
+            onlyIfTrusted : true,
+          })
+          console.log("Connected to Public Key : ",
+            response.publicKey.toString()
+          )
+          setWalletAddress(response.publicKey.toString())
+        }
+      }else{
+        alert("Solana object not found! Get a phantom wallet")
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const connectWallet = async() => {
+    const { solana } = window;
+    if(solana){
+      const response = await solana.connect();
+      console.log("Connected to Public Key : ",
+            response.publicKey.toString()
+      )
+      setWalletAddress(response.publicKey.toString())
+    }
+  }
+
+  useEffect(() => {
+    const onLoad = async() => {
+      await checkIfWalletConnected();
+    }
+    window.addEventListener('load', onLoad)
+    return () => window.removeEventListener('load', onLoad)
+  },[])
+
+  const renderNotConnectedContainer = () => {
+    return <button onClick = {connectWallet}> Connect Wallet </button>
+  }
+
+  const renderConnectedContainer = () => {
+    return <></>
+  }
+
+  return(
+    <>
+      {(!walletAddress && renderNotConnectedContainer()) || renderConnectedContainer()}
+    </>
   );
 }
 
